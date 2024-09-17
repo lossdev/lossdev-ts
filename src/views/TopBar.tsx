5fe6bc9b0744aeb6b73b8799e250ac5e41010ffc
lossdev <28501735+lossdev@users.noreply.github.com>
2024-09-16 22:24:43 -0600

    remove unneeded div

--- src/views/TopBar.tsx
+++ src/views/TopBar.tsx
@@ -7,7 +7,6 @@ export const TopBar = ({children}: React.PropsWithChildren<{}>) => {
             <div className="flex h-full content-center items-center">
                 <h1 className="font-jbmono-bold tracking-tight text-slate-50 text-4xl md:text-5xl ml-5 md:ml-32">lossdev.com</h1>
             </div>
-            <div className="flex flex-row-reverse h-full w-2/5 content-center"></div>
         </div>
     </React.Fragment>
     );
